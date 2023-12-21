b002a0eb649b8975f1950a7600bc4be6c9918416
jingpeng <jingpeng.wang@outlook.com>
2023-12-21 16:47:37 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -17,16 +17,10 @@ cd $CURRENT_DIR && sh bundle.sh
 ## JS
 cd $CURRENT_DIR/doric-js && npm version $CURRENT_VERSION --allow-same-version
 
-## Android
-echo "version="$CURRENT_VERSION > $CURRENT_DIR/doric-android/version.properties
-
 ## iOS
 sed -i "" "s/\(version[ ]*= \)'[0-9 \.]*'/\1'$CURRENT_VERSION'/g" $CURRENT_DIR/DoricCore.podspec
 
 sed -i "" "s/\(version[ ]*= \)'[0-9 \.]*'/\1'$CURRENT_VERSION'/g" $CURRENT_DIR/DoricDevkit.podspec
-
-## Web
-cd $CURRENT_DIR/doric-web && npm version $CURRENT_VERSION --allow-same-version
 
 ## CLI
 echo $CURRENT_VERSION > $CURRENT_DIR/doric-cli/assets/version
@@ -48,11 +42,5 @@ git push --tags
 echo "Publish CLI"
 cd $CURRENT_DIR/doric-cli && npm publish 
 
-echo "Publish JS"
-cd $CURRENT_DIR/doric-js && npm publish 
-echo "Publish Web"
-cd $CURRENT_DIR/doric-web && npm publish 
-echo "Publish Android"
-cd $CURRENT_DIR/doric-android && ./gradlew clean publishAll 
 echo "Publish iOS"
-cd $CURRENT_DIR && pod trunk push DoricCore.podspec --allow-warnings && pod trunk push DoricDevkit.podspec --allow-warnings
+cd $CURRENT_DIR && pod lib lint DoricCore.podspec --allow-warnings && pod lib lint DoricDevkit.podspec --allow-warnings
